55abc38d45f7023a40a15d4bf2ed606f9de41947
Ezekiel Eromosei <ezekiel@1funnel.ng>
2026-01-25 16:34:21 +0100

    first commit

---
+++ src/main/java/com/zee/courseauthdemo/CourseAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zee.courseauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAuthDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zee/courseauthdemo/CourseAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.courseauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAuthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
